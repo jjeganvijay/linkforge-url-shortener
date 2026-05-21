e3ef4bbbf153960a8d7d9e88573e8075f2ff63f3
jjeganvijay <jjeganvijay@gmail.com>
2026-05-21 10:54:04 +0530

    Cascade delete links/visits when deleting a user

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -37,4 +37,39 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const cascadeDeleteUserData = async (userId) => {
+  if (!userId) return;
+  const Link = require('./Link');
+  const Visit = require('./Visit');
+
+  const links = await Link.find({ userId }, { _id: 1 }).lean();
+  const linkIds = links.map((l) => l._id);
+
+  if (linkIds.length > 0) {
+    await Visit.deleteMany({ linkId: { $in: linkIds } });
+  }
+  await Link.deleteMany({ userId });
+};
+
+// Cascade deletes when deleting via Mongoose model APIs.
+userSchema.pre('deleteOne', { document: true, query: false }, async function () {
+  await cascadeDeleteUserData(this._id);
+});
+
+userSchema.pre('findOneAndDelete', async function () {
+  const filter = this.getFilter();
+  const user = await this.model.findOne(filter, { _id: 1 }).lean();
+  if (user?._id) {
+    await cascadeDeleteUserData(user._id);
+  }
+});
+
+userSchema.pre('deleteOne', { document: false, query: true }, async function () {
+  const filter = this.getFilter();
+  const user = await this.model.findOne(filter, { _id: 1 }).lean();
+  if (user?._id) {
+    await cascadeDeleteUserData(user._id);
+  }
+});
+
 module.exports = mongoose.model('User', userSchema);
